2ae10bd996e9c00e1a27aa7470c03ae311dbbf34
Mrityunjay112358 <73526816+Mrityunjay112358@users.noreply.github.com>
2021-05-18 19:24:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+song1 = "";
+song2 = "";
+
+song1_status = "";
+song2_status = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+	song1 = loadSound("hedwigs_theme.mp3");
+	song2 = loadSound("ill_be_there_for_you.mp3");
+}
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(){
+
+}
